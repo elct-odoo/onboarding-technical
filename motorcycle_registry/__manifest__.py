f3fc2da0d413193ccd9a18ebfe1b371bbace8a15
Elijah Tay <elct@odoo.com>
2024-02-28 12:37:49 -0500

    [ADD] motorcycle_registry module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Motorcycle Registry",
+
+    'summary': """
+        Track the Motorcycle Registration and Ownership of each motorcycle of the brand""",
+
+    'description': """
+        Custom development for Kawill Motorcycles, a motorcycle manufacturer based in Mexico City. This app aims to keep track of the Motorcycle Registration and Ownership of each motorcycle of the brand.
+    """,
+    'license': 'OPL-1',
+    'author': "elct",
+    'website': "https://www.odoo.com",
+
+    'category': 'Kawiil/',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'application': True,
+}
