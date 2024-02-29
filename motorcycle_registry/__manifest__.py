948009248a47f4aa7b32d005d456971cedad860a
Elijah Tay <elct@odoo.com>
2024-02-29 13:30:50 -0500

    [ADD] Form View

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -15,8 +15,8 @@
     "data": [
         "security/motorcycle_registry_groups.xml",
         "security/ir.model.access.csv",
-        "views/views.xml",
-        "views/templates.xml",
+        "views/motorcycle_registry_menuitems.xml",
+        "views/motorcycle_registry_views.xml",
     ],
     "demo": [
         "demo/motorcycle_registry_demo.xml",
